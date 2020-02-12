f09871e72dfab0db597d28ee254dcb8dbc565d11
martin.grofcik <mgrofcik@gmail.com>
2020-02-12 16:32:07 +0100

    test get/set variable in the task scope

--- flowable-groovy/src/test/java/org/flowable/ex/groovy/dsl/VariableTest.java
+++ flowable-groovy/src/test/java/org/flowable/ex/groovy/dsl/VariableTest.java
@@ -7,13 +7,14 @@ import java.util.Collections;
 import org.flowable.engine.impl.test.PluggableFlowableTestCase;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.test.Deployment;
+import org.flowable.task.api.Task;
 import org.junit.jupiter.api.Test;
 
 public class VariableTest extends PluggableFlowableTestCase {
 
     @Test
     @Deployment
-    void getSetVariableWithMetaClassExtension() {
+    void getSetVariableInExecutionWithMetaClassExtension() {
         ProcessInstance pi = runtimeService.createProcessInstanceBuilder().
                 processDefinitionKey("scriptTaskProcess").
                 variables(Collections.singletonMap("inputVariable", "inputVariableValue")).
@@ -21,6 +22,19 @@ public class VariableTest extends PluggableFlowableTestCase {
 
         assertThat(runtimeService.hasVariable(pi.getId(), "newVariable")).isTrue();
         assertThat(runtimeService.getVariable(pi.getId(), "newVariable")).isEqualTo("inputVariableValue");
+    }
+
+    @Test
+    @Deployment
+    void getSetVariableInTaskScopeWithMetaClassExtension() {
+        ProcessInstance pi = runtimeService.createProcessInstanceBuilder().
+                processDefinitionKey("userTaskWithScriptListenerProcess").
+                variables(Collections.singletonMap("inputVariable", "inputVariableValue")).
+                start();
+        Task task = taskService.createTaskQuery().includeTaskLocalVariables().processInstanceId(pi.getId()).singleResult();
+
+        assertThat(taskService.hasVariable(task.getId(), "newVariable")).isTrue();
+        assertThat(taskService.getVariable(task.getId(), "newVariable")).isEqualTo("inputVariableValue");
     }
 
 }
